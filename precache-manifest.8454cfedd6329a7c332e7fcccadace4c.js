ec31722651ee78088b9cf6576ce7e1e0bdc48316
Rye <19521087@gm.uit.edu.vn>
2020-10-18 05:13:12 +0700

    Updates

---
+++ precache-manifest.8454cfedd6329a7c332e7fcccadace4c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e7ea758c2b36a6a8416796969c9051",
+    "url": "/hommier/index.html"
+  },
+  {
+    "revision": "d8999a40326ce4ec060a",
+    "url": "/hommier/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "005be7d3a519d2f9d8f1",
+    "url": "/hommier/static/css/main.4875fc82.chunk.css"
+  },
+  {
+    "revision": "d8999a40326ce4ec060a",
+    "url": "/hommier/static/js/2.9bccbc33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hommier/static/js/2.9bccbc33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "005be7d3a519d2f9d8f1",
+    "url": "/hommier/static/js/main.09204354.chunk.js"
+  },
+  {
+    "revision": "e734811238ed3385164a",
+    "url": "/hommier/static/js/runtime-main.d0380ba7.js"
+  },
+  {
+    "revision": "29fae3d091796477fd3a6072629aca84",
+    "url": "/hommier/static/media/about-img.29fae3d0.jpg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/hommier/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/hommier/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/hommier/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/hommier/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/hommier/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/hommier/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/hommier/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/hommier/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/hommier/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/hommier/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/hommier/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/hommier/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/hommier/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/hommier/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/hommier/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "8f338c7b1bf7e4e736c7173228849b8a",
+    "url": "/hommier/static/media/intro-bg.8f338c7b.jpg"
+  },
+  {
+    "revision": "3ed5cc2aa647fb1da899b4166f8e1214",
+    "url": "/hommier/static/media/intro-img.3ed5cc2a.png"
+  },
+  {
+    "revision": "cecf94026cef166fceda9ad8e49bad80",
+    "url": "/hommier/static/media/testimonial-2.cecf9402.jpg"
+  }
+]);
